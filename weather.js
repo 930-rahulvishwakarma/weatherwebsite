b224183749e4c792c4f7de1f5d84646a07e550c3
930-rahulvishwakarma <113338505+930-rahulvishwakarma@users.noreply.github.com>
2023-07-13 17:51:46 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,11 +1,8 @@
 
-/* api key */
-/* https://api.openweathermap.org/data/2.5/weather?q=giridih&appid=578b7153ba1316232303203add048620&units=metric */
 
 const apiurl = " https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const apikey = "578b7153ba1316232303203add048620";
 
-// const api          = 'https://api.openweathermap.org/data/2.5/weather?q=giridih&appid=578b7153ba1316232303203add048620&units=metric'; 
 const search_input = document.querySelector('.search > input');
 const search = document.querySelector('.search > span');
 const temp = document.querySelector('.temp');
@@ -28,32 +25,32 @@ const Get_temp = async (city) => {
     console.log(data);
 
     if (data.main.temp < 20) {
-      img.src = "weather/cold.jpg";
+      img.src = "cold.jpg";
       temp.style.color = "grey";
       area.style.color = "grey";
       search_input.style.color = "grey"
     }
    else if (data.weather[0].main == 'Haze' || data.weather[0].main == "Clouds") {
-      img.src = "weather/weather_backgd.jpg";
+      img.src = "weather_backgd.jpg";
       temp.style.color = "#ffffffcc";
       area.style.color = "#ffffffcc";
       search_input.style.color = "white"
 
     }
     else if (data.weather[0].main == "Thunderstorm" || data.weather[0].main == "Rain") {
-      img.src = "weather/rainy.png";
+      img.src = "rainy.png";
       temp.style.color = "#ffffffcc";
       area.style.color = "#ffffffcc";
       search_input.style.color = "white"
 
     }
     else if (data.weather[0].main == "cold") {
-      img.src = "weather/cold.jpg";
+      img.src = "cold.jpg";
       temp.style.color = "black";
       area.style.color = "black";
     }
     else if (data.weather[0].main == 'Mist') {
-      img.src = "weather/mint.jpeg";
+      img.src = "mint.jpeg";
       temp.style.color = "#ffffffcc";
       area.style.color = "#ffffffcc";
       search_input.style.color = "white"
